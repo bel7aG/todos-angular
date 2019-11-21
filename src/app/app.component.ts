1f6520a33da67d783f3d555c715f6a881bd506f2
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-11-22 00:55:23 +0100

    first touchs in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todos';
+  appTitle: string = `todos`;
+  constructor() {
+    console.log("123");
+  }
 }
