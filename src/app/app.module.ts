3c69c189b7f559da26a49ee819a974c872ec2c7d
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-11-22 01:39:53 +0100

    create todo component and loop it from todos component and pass todo from todos as props (Input decorator In angular) and use it in todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule
