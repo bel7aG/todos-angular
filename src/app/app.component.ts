698b344f3fe9a670d0da1b64701ed10f438e6655
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-11-22 01:10:41 +0100

    create a Todo type in (models) and use it in Todos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  appTitle: string = `todos`;
+  appTitle: string = 'todos';
   constructor() {
-    console.log("123");
+    console.log('first todos application with Angular (EXCITED!).');
   }
 }
