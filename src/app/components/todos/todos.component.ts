e0bb7241a1c56f47b4ccf50676a13131d8276e06
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-11-22 01:24:42 +0100

    some todos

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../../modules';
+import { Todo } from '../../models';
 
 @Component({
   selector: 'app-todos',
@@ -11,6 +11,32 @@ export class TodosComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    alert('is this componentsDidMount ?');
+    this.todos = [
+      {
+        id: 1,
+        title: 'first todo',
+        description: 'first todo is the best todo!',
+        completed: false
+      },
+      {
+        id: 2,
+        title: ' todo 2',
+        description: 'first todo is the best todo!',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'todo 3',
+        description: 'first todo is the best todo!',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'todo 4',
+        description: 'first todo is the best todo!',
+        completed: false
+      }
+    ];
+    console.log(this.todos);
   }
 }
